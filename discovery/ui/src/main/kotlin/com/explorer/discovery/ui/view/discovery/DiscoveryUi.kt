29031ccf0de63616cce8af683ac005a7689eb9b4
conxtantyn <constantine@siliconbear.com>
2025-01-02 00:18:48 +0100

    chore(project): initialize

--- discovery/ui/src/main/kotlin/com/explorer/discovery/ui/view/discovery/DiscoveryUi.kt
+++ discovery/ui/src/main/kotlin/com/explorer/discovery/ui/view/discovery/DiscoveryUi.kt
@@ -1,13 +1,18 @@
 package com.explorer.discovery.ui.view.discovery
 
+import com.explorer.core.ui.base.UiBuilder
 import com.explorer.core.ui.base.UiComponent
 import com.explorer.core.ui.base.UiModule
-import com.explorer.core.ui.base.UiViewModel
 import com.explorer.discovery.domain.repository.AdvertiserRepository
 import com.explorer.discovery.domain.repository.ConsumerRepository
 import com.explorer.discovery.ui.view.advertiser.AdvertiserUi
+import com.explorer.discovery.ui.view.advertiser.AdvertiserUiBuilder
 import com.explorer.discovery.ui.view.consumer.ConsumerUi
+import com.explorer.discovery.ui.view.consumer.ConsumerUiBuilder
 import dagger.BindsInstance
+import dagger.Module
+import dagger.Provides
+import dagger.multibindings.IntoMap
 
 interface DiscoveryUi {
     fun advertiser(): AdvertiserRepository
@@ -18,16 +23,34 @@ interface DiscoveryUi {
     @Retention(AnnotationRetention.RUNTIME)
     annotation class Scope
 
+    @Module
+    object Provider {
+        @Scope
+        @Provides
+        @IntoMap
+        @UiBuilder.Bind(AdvertiserUiBuilder::class)
+        fun provideAdvertiserUiBuilder(component: Component): UiBuilder {
+            return AdvertiserUiBuilder(component)
+        }
+
+        @Scope
+        @Provides
+        @IntoMap
+        @UiBuilder.Bind(ConsumerUiBuilder::class)
+        fun provideConsumerUiBuilder(component: Component): UiBuilder {
+            return ConsumerUiBuilder(component)
+        }
+    }
+
     @Scope
     @dagger.Component(
         dependencies = [ DiscoveryUi::class ],
-        modules = [ UiModule::class, DiscoveryUiModule::class ]
+        modules = [ UiModule::class, Provider::class ]
     )
     interface Component : DiscoveryUi,
         UiComponent,
         AdvertiserUi,
-        ConsumerUi,
-        UiViewModel {
+        ConsumerUi {
         @dagger.Component.Builder
         interface Builder {
             fun discoveryUi(discoveryUi: DiscoveryUi): Builder
